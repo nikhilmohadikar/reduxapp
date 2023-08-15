191e7a3c81242cbc8d446e00e51a752a9b38f548
Nikhil Mohadikar <nikumohadikar1@gmail.com>
2023-08-15 17:42:47 +0530

    Navbar Creation and Shop file Creating

---
+++ src/componant/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <div className='container my-5'>
+            <h2>Add Nike Shoes 50 Rs</h2>
+            <div className="btn btn-primary mx-2">-</div>
+                Add item to the cart
+            <div className="btn btn-primary mx-2">+</div>
+        </div>
+    </div>
+  )
+}
+
+export default Shop
